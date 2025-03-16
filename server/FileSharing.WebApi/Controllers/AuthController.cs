6f315ed314d250a9a5aed0351838b54183dfe1df
Dmitry Korobeyshchikov <korobeyshchikov.d@mail.ru>
2025-03-16 20:22:20 +0300

    Added auth controller and register method

---
+++ server/FileSharing.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using FileSharing.WebApi.Entities;
+using FileSharing.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSharing.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDTO request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+    }
+}
